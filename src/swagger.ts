ea5a64cadc8b34feafec1c8d8d2230da92fad5db
Ajayi favour <ajayifavour81@gmail.com>
2024-07-09 14:31:06 +0100

    new changes from swagger

--- src/swagger.ts
+++ src/swagger.ts
@@ -24,7 +24,7 @@ const options = {
                             default: "example@example.com",
                             description: "User's email"
                         },
-                        password:{
+                        password:{ 
                             type: "string",
                             format: "password",
                             description: "User's password",
