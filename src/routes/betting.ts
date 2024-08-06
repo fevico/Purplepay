28ac331a2ea108457448dff1401b3e651abf1de7
Ajayi favour <ajayifavour81@gmail.com>
2024-08-06 18:19:01 +0100

    latest update from betting

--- src/routes/betting.ts
+++ src/routes/betting.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { bettingTopUp, getBettingProvider, validateCustormerId } from "src/controller/betting";
+import { isAuth } from "src/middleware/auth";
 
 const bettingRouter = Router()
 
-bettingRouter.get("/betting-providers", getBettingProvider)
-bettingRouter.post("/validate-customerId", validateCustormerId)
-bettingRouter.post("/top-up", bettingTopUp)
+bettingRouter.get("/betting-providers", isAuth, getBettingProvider)
+bettingRouter.post("/validate-customerId", isAuth, validateCustormerId)
+bettingRouter.post("/top-up", isAuth, bettingTopUp)
 
 export default bettingRouter;
